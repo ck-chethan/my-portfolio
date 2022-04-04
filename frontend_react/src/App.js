600a3f5f061b4cd0bd528c726a9a14e19aa965c4
chethanck <chethanckm66@gmail.com>
2022-04-04 22:48:25 +0530

    Backend server using sanity and Frontend starting files

---
+++ frontend_react/src/App.js
@@ -0,0 +1,16 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
